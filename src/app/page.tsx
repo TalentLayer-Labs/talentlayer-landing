f03f1c952b655b9069947cdace033fbfae93ec3b
mattiapomelli <mattiapomelli@gmail.com>
2023-08-12 20:06:17 +0200

    Add integration section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "@/components/hero";
+import { IntegrationsSection } from "@/components/integrations-section";
 import { IntroducationSecion } from "@/components/introduction-section";
 import { ModulesSection } from "@/components/modules-section";
 import { VisionSection } from "@/components/vision-section";
@@ -10,6 +11,7 @@ export default function Home() {
       <IntroducationSecion />
       <ModulesSection />
       <VisionSection />
+      <IntegrationsSection />
     </>
   );
 }
