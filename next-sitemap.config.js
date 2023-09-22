e0864e7a3b5c344927283a90787d479eeb0d7a87
Romain <romain@talentlayer.org>
2023-09-22 15:31:20 +0200

    fix sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://localhost:3000",
+  siteUrl: "https://www.talentlayer.org:3000",
   generateRobotsTxt: true,
   generateIndexSitemap: false,
 };
