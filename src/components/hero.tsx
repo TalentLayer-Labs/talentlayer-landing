ddb5868bbf54afab7b9bf9941fb5feb4c6d84a2f
KPomales <kirstenpomales@gmail.com>
2024-02-14 17:32:25 -0800

    new edits

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -11,12 +11,12 @@ export const Hero = () => {
       <Container className="relative flex flex-col items-center gap-20 pb-40 pt-14 sm:pt-40 md:flex-row">
         <div className="order-2 flex flex-col gap-10">
           <h1 className="text-5xl font-bold sm:text-6xl">
-            building <a className="text-primary">work tech</a> doesn&apos;t have to be{" "}
-            <span className="font-bold italic text-primary">hard work</span>.
+            the global <a className="text-primary">network layer</a> for the{" "}
+            <span className="font-bold text-primary">service economy</span>.
           </h1>
           <p className="max-w-[40rem] text-xl text-base-content-neutral sm:max-w-[40rem]">
             TalentLayer is an 🌐 open protocol and ⚒️ developer toolkit for building better hiring
-            platforms.
+            marketplaces
           </p>
 
           <div className="flex flex-col gap-4 min-[500px]:flex-row">
